da8579467393613f6cc85a6cff99100016c36f58
nguyennghi3489 <macpro@MacBook-Pro-Retina.local>
2015-01-22 20:13:33 +0700

    Update

--- js/common.js
+++ js/common.js
@@ -1,8 +1,35 @@
 $(function(){
-	$('.filter ul > li ul').click(function(e){
+	$('.filter>ul > li > ul').click(function(e){
 	}).hide();
-	$('.filter ul li').click(function(e){
-		e.preventDefault();
-		e.stopPropagation();
-	})
+	if($(window).width() > 1280){
+		$('.filter>ul>li').unbind();
+		$('.filter>ul>li').bind({
+			click:function(){
+				var selfClick = $(this).find('ul').is(':visible');
+				if(selfClick){
+					$(this).parent().find('>li ul:visible').slideToggle();
+					return;
+				}
+				$(this).parent().find('>li ul:visible').slideToggle();
+				$(this).find('ul').slideToggle();
+			}
+		})
+	}else if($(window).width() <=1280 ){
+		$('.filter>ul>li').unbind();
+		$('.filter>ul>li').bind({
+			mouseenter:function(e){
+				$(this)
+				.find('ul')
+				.stop(true,true)
+				.slideDown('fast');
+			},
+			mouseleave:function(e){
+				$(this)
+				.find('ul')
+				.stop(true,true)
+				.fadeOut('fast');
+			},
+		});
+	} 
+	
 })
