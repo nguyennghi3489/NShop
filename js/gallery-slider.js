2118baa44cc0cf2ed1e35d977fbaf6512822239e
nguyennghi3489 <nguyennghi3489@gmail.com>
2015-02-09 14:53:13 +0700

    Update New Feature

--- js/gallery-slider.js
+++ js/gallery-slider.js
@@ -1,17 +1,17 @@
 var App = App || {};
 App.Templates = {};
 App.Templates.gallery = [
-	'<div class="nwrapper" style="width:<%= opts.width %>px;height:<%= opts.height %>px;overflow:hidden;position:relative;">',
-		'<ul class="nslider" style="width:<%=imageList.length * opts.width %>px<% %>;height:<%= opts.height %>px;position:absolute;top:0px;">',
-			'<% _.each(imageList,function(i){ %><li style="width:<%= opts.width %>px;height:100%;background-image:url(<%= i %>);background-size: 100%;float:left"></li> <% }) %>',
+	'<div class="nwrapper" style="width:<%= opts.width %>px;height:<%= opts.width*opts.ratio %>px;overflow:hidden;position:relative;">',
+		'<ul class="nslider" style="width:<%=imageList.length * opts.width %>px<% %>;height:<%= opts.width*opts.ratio %>px;position:absolute;top:0px;">',
+			'<% _.each(imageList,function(i){ %><li style="width:<%= opts.width %>px;height:100%;background-image:url(<%= i %>);background-repeat:none;background-size: 100%;float:left"></li> <% }) %>',
 		'</ul>',
 	'</div>',
-	'<div class="control">',
+	'<div class="control" style="width:<%= opts.width %>px;">',
 		'<a class="prev" href="#"></a>',
 		'<a class="next" href="#"></a>',
 	'</div>',
 	'<div class="thumbs">',
-		'<ul class="nthumbslider">',
+		'<ul class="nthumbslider square">',
 			'<% _.each(imageList,function(item, index){ %><li><a data-index="<%= index %>" href=""></a></li> <% }) %>',
 		'</ul>',
 	'</div>'
@@ -111,6 +111,7 @@ App.Templates.gallery = [
 		auto    : 0,
 		width	: 1000,
 		height	: 500,
+		ratio: 1/2,
 		thumbWidth:70,
 		thumbHeight:50,
 		easing:'easeOutCubic',
