a466a380f64f1de7fcf1a7fb64c6d25967d8c83e
nguyennghi3489 <nguyennghi3489@gmail.com>
2015-02-10 17:11:09 +0700

    Update Navigation
    Update

--- js/common.js
+++ js/common.js
@@ -1,22 +1,23 @@
 $(function(){
-	$('.filter>ul > li > ul').click(function(e){
+	$('.nav-wrapper ul > li > ul').click(function(e){
 	}).hide();
-	if($(window).width() > 1280 || $(window).width() <= 640){
-		$('.filter>ul>li').unbind();
-		$('.filter>ul>li').bind({
-			click:function(){
+	if($(window).width() <= 640){
+		$('.nav-wrapper ul>li').unbind();
+		$('.nav-wrapper ul>li, .nav-wrapper > li > ul > li').bind({
+			click:function(e){
+				e.stopPropagation();
 				var selfClick = $(this).find('ul').is(':visible');
 				if(selfClick){
 					$(this).parent().find('>li ul:visible').slideToggle('fast');
 					return;
 				}
 				$(this).parent().find('>li ul:visible').slideToggle('fast');
-				$(this).find('ul').slideToggle('fast');
+				$(this).find('ul:first').slideToggle('fast');
 			}
 		})
-	}else if($(window).width() <=1280){
-		$('.filter>ul>li').unbind();
-		$('.filter>ul>li').bind({
+	}else{
+		$('.nav-wrapper>nav>ul>li').unbind();
+		$('.nav-wrapper>nav>ul>li').bind({
 			mouseenter:function(e){
 				$(this)
 				.find('ul')
@@ -26,10 +27,9 @@ $(function(){
 			mouseleave:function(e){
 				$(this)
 				.find('ul')
-				.stop(true,true)
+				.delay(200)
 				.fadeOut('fast');
 			},
 		});
-	} 
-	
+	}
 })
